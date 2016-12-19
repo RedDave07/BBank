8a961b912425abf8d1881fd9e0bc970f72cc43a3
David <David@LAPTOP-DAVID>
2016-12-20 00:04:13 +0200

    new commit

--- src/bbank/Client.java
+++ src/bbank/Client.java
@@ -1,6 +1,8 @@
 
 package bbank;
 
+import java.util.Date;
+
 /**
  *
  * @author ToniGeorge
@@ -8,51 +10,32 @@ package bbank;
 public abstract class Client {
     String Name ;
     String PhoneNum ;
-    Blood BloodType;
-    Client(String N,String PN,Blood BT){
+    String BloodType;
+    
+    Client(String N,String PN,String BT){
     this.Name=N;
     this.PhoneNum=PN;
     this.BloodType=BT;
     }
     
-public void Request(){
-    
-    
-}
-public void GetBloodInformation(){
-    switch(BloodType.getClass().getName()){
-        case "BloodA":
-             System.out.println("Blood Type : A");
-            break;
-        case "BloodB":
-             System.out.println("Blood Type : B");
-            break;
-        case "BloodO":
-             System.out.println("Blood Type : O");
-            break;
-            
-    }
-    System.out.println("amount: "+BloodType.amount+" "+"accepted: "+BloodType.disease+" "+"haemoglobin: "+BloodType.haemoglobin);
-
-}
 }
 class Donor extends Client{
     Boolean diseases ;
-    Donor(String N,String PN,Blood BT,Boolean diseases){
+    Date Last_donate;
+    
+    Donor(String N,String PN,String BT,Boolean diseases, Date last_donate){
     super( N,PN,BT);
+    this.Last_donate = last_donate;
     this.diseases=diseases;
     }
 
-
-
 }
 class Recipient extends Client{
-   float quantity_needed ;
-    Recipient(String N,String PN,Blood BT,float QN){
+    float quantity_needed ;
+    
+    Recipient(String N,String PN,String BT,float QN){
     super( N,PN,BT);
     this.quantity_needed=QN;
     }
-
-
 
 }
