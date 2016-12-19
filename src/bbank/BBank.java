87fe60ff38bccfc99a0f38d000fdcedc3b6ad7d7
David <David@LAPTOP-DAVID>
2016-12-19 23:36:57 +0200

    new commit

--- src/bbank/BBank.java
+++ src/bbank/BBank.java
@@ -1,35 +1,30 @@
 package bbank;
 
-import java.util.Date;
-
 public class BBank {
 
     public static void main(String[] args) {
        Window w = new Window();
        w.setVisible(true);
         
-        
-       System.out.println("5ODO KOLOKO _|_");
-       Date d = new Date();
        
-//       BloodA a1 = new BloodA(5);
-//       BloodA a2 = new BloodA(8);
-//       BloodStock.blood_A.add(a1);
-//       BloodStock.blood_A.add(a2);
-//       for(int i=0; i<BloodStock.blood_A.size(); i++){
-//           System.out.println("Blood A: "+BloodStock.blood_A.get(i).amount);
-//       }
-//       System.out.println("Stock size "+BloodStock.blood_A.size());
-//       
-//       BloodStock.saveListToFile(BloodStock.blood_A, "BloodA");
-//       BloodStock.blood_A.clear();
-//       System.out.println("Stock size "+BloodStock.blood_A.size());
-//       
-//       BloodStock.blood_A = BloodStock.loadListFromFile("BloodA.json");
-//       System.out.println("Stock size "+BloodStock.blood_A.size());
-//       for(int i=0; i<BloodStock.blood_A.size(); i++){
-//           System.out.println("Blood A: "+BloodStock.blood_A.get(i).amount);
-//       }
+       BloodA a1 = new BloodA(5);
+       BloodA a2 = new BloodA(8);
+       BloodStock.blood_A.add(a1);
+       BloodStock.blood_A.add(a2);
+       for(int i=0; i<BloodStock.blood_A.size(); i++){
+           System.out.println("Blood A: "+BloodStock.blood_A.get(i).amount);
+       }
+       System.out.println("Stock size "+BloodStock.blood_A.size());
+       
+       BloodStock.saveListToFile(BloodStock.blood_A, "BloodA");
+       BloodStock.blood_A.clear();
+       System.out.println("Stock size "+BloodStock.blood_A.size());
+       
+       BloodStock.blood_A = BloodStock.loadListFromFile("BloodA.json");
+       System.out.println("Stock size "+BloodStock.blood_A.size());
+       for(int i=0; i<BloodStock.blood_A.size(); i++){
+           System.out.println("Blood A: "+BloodStock.blood_A.get(i).amount);
+       }
        
     }
     
