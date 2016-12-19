4c68e2328df29265238bc1a72f04cf31babbe6a4
David <David@LAPTOP-DAVID>
2016-12-20 00:05:36 +0200

    new commit

---
+++ src/bbank/ClientDataBase.java
@@ -0,0 +1,12 @@
+package bbank;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author David
+ */
+public class ClientDataBase {
+    LinkedList Donors_list = new LinkedList ();
+    LinkedList Recipients_list = new LinkedList ();
+}
