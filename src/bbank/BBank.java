b3b38b10366ada0db14750e518d72f237d3b42ec
David <David@LAPTOP-DAVID>
2016-12-19 23:01:39 +0200

    new commit

--- src/bbank/BBank.java
+++ src/bbank/BBank.java
@@ -14,22 +14,22 @@ public class BBank {
        
 //       BloodA a1 = new BloodA(5);
 //       BloodA a2 = new BloodA(8);
-       BloodStock.blood_A.add(a1);
-       BloodStock.blood_A.add(a2);
-       for(int i=0; i<BloodStock.blood_A.size(); i++){
-           System.out.println("Blood A: "+BloodStock.blood_A.get(i).amount);
-       }
-       System.out.println("Stock size "+BloodStock.blood_A.size());
-       
-       BloodStock.saveListToFile(BloodStock.blood_A, "BloodA");
-       BloodStock.blood_A.clear();
-       System.out.println("Stock size "+BloodStock.blood_A.size());
-       
-       BloodStock.blood_A = BloodStock.loadListFromFile("BloodA.json");
-       System.out.println("Stock size "+BloodStock.blood_A.size());
-       for(int i=0; i<BloodStock.blood_A.size(); i++){
-           System.out.println("Blood A: "+BloodStock.blood_A.get(i).amount);
-       }
+//       BloodStock.blood_A.add(a1);
+//       BloodStock.blood_A.add(a2);
+//       for(int i=0; i<BloodStock.blood_A.size(); i++){
+//           System.out.println("Blood A: "+BloodStock.blood_A.get(i).amount);
+//       }
+//       System.out.println("Stock size "+BloodStock.blood_A.size());
+//       
+//       BloodStock.saveListToFile(BloodStock.blood_A, "BloodA");
+//       BloodStock.blood_A.clear();
+//       System.out.println("Stock size "+BloodStock.blood_A.size());
+//       
+//       BloodStock.blood_A = BloodStock.loadListFromFile("BloodA.json");
+//       System.out.println("Stock size "+BloodStock.blood_A.size());
+//       for(int i=0; i<BloodStock.blood_A.size(); i++){
+//           System.out.println("Blood A: "+BloodStock.blood_A.get(i).amount);
+//       }
        
     }
     
