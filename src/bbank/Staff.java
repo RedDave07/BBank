a42011460185c2ac318acf64bfa7dfc6371bd5d8
David <David@LAPTOP-DAVID>
2016-12-19 22:02:16 +0200

    new commit

---
+++ src/bbank/Staff.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package staff;
+
+import java.util.LinkedList;
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class Staff {
+    
+    
+    private String name;
+    private String phoneNumber;
+    private String ID;
+    private String password;
+    LinkedList clients = new LinkedList();
+    
+    void registerClient()
+    {
+    }
+    boolean checkDonorBloodSample()
+    {return true;}
+    void ViewStatistics()
+    {
+        
+    }
+    void ViewWarnings()
+    {}
+    boolean checkStockAvailability()
+    {
+      return true;  
+    }
+    void ControlStock()
+    {
+    }
+    
+            
+}
