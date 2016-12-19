a0be3b774bdd7c61a553090231029d6f80e02bf2
David <David@LAPTOP-DAVID>
2016-12-19 23:00:45 +0200

    new commit

--- src/bbank/Blood.java
+++ src/bbank/Blood.java
@@ -1,83 +1,62 @@
 package bbank;
 
+import java.util.Date;
+
 
 public abstract class Blood {
     //Donor person;
     double amount;
     int haemoglobin;
-    boolean disease;
+    Date date;
     
     
-    
-   Blood (double amount, int haemoglobin, boolean disease){
+   Blood (double amount, int haemoglobin, Date date){
        this.amount = amount;
        this.haemoglobin = haemoglobin;
-       this.disease = disease;
+       this.date = date;
    }
    
-   Blood (double amount, boolean disease){
+   Blood (double amount, Date date){
        this.amount = amount;
-       this.disease = disease;
-   }
-   
-   Blood (double amount){
-       this.amount = amount;
+       this.date = date;
    }
     
-   public abstract void setNotifier ();
+   public void setNotifier (){
+       
+   }
 }
 
 class BloodA extends Blood {
     
-    public BloodA(double amount, int haemoglobin, boolean disease) {
-        super(amount, haemoglobin, disease);
+    public BloodA(double amount, int haemoglobin, boolean disease, Date date) {
+        super(amount, haemoglobin, date);
     }
-    public BloodA (double amount, boolean disease){
-        super(amount, disease);
-    }
-    public BloodA (double amount){
-        super(amount);
-    }
-
-    @Override
-    public void setNotifier() {
-        
+    public BloodA (double amount, Date date){
+        super(amount, date);
     }
     
 }
 
 class BloodB extends Blood {
     
-    public BloodB(double amount, int haemoglobin, boolean disease) {
-        super(amount, haemoglobin, disease);
+    public BloodB(double amount, int haemoglobin, Date date) {
+        super(amount, haemoglobin, date);
     }
-    public BloodB (double amount, boolean disease){
-        super(amount, disease);
-    }
-    public BloodB (double amount){
-        super(amount);
+    public BloodB(double amount, Date date){
+        super(amount, date);
     }
 
-    @Override
-    public void setNotifier() {
-        
-    }
+
 }
 
 class BloodO extends Blood {
     
-    public BloodO(double amount, int haemoglobin, boolean disease) {
-        super(amount, haemoglobin, disease);
+    public BloodO(double amount, int haemoglobin, Date date) {
+        super(amount, haemoglobin, date);
     }
-    public BloodO (double amount, boolean disease){
-        super(amount, disease);
-    }
-    public BloodO (double amount){
-        super(amount);
+    public BloodO(double amount, Date date){
+        super(amount, date);
     }
 
-    @Override
-    public void setNotifier() {
-        
-    }
+
 }
