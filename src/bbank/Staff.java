f9813f361a2bf48ed3640432123dbda6c873f081
David <David@LAPTOP-DAVID>
2016-12-19 22:10:16 +0200

    new commit

--- src/bbank/Staff.java
+++ src/bbank/Staff.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package staff;
+package bbank;
 
 import java.util.LinkedList;
 import com.google.gson.Gson;
