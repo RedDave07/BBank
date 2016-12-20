6adc6f97e0935141fa18320c13fe0e0f46863097
David <David@LAPTOP-DAVID>
2016-12-20 23:56:48 +0200

    save & loading are working now

--- src/bbank/ClientDataBase.java
+++ src/bbank/ClientDataBase.java
@@ -1,5 +1,11 @@
 package bbank;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.lang.reflect.Type;
 import java.util.LinkedList;
 
 /**
@@ -7,6 +13,58 @@ import java.util.LinkedList;
  * @author David
  */
 public class ClientDataBase {
-    LinkedList Donors_list = new LinkedList ();
-    LinkedList Recipients_list = new LinkedList ();
+    public static LinkedList <Donor> Donors_list = new LinkedList ();
+    public static LinkedList <Recipient> Recipients_list = new LinkedList ();
+    
+    public static void saveList(LinkedList L, String ClientType){
+        Gson gson = new Gson ();
+        Type type;
+        System.out.println("saving to file "+""+ClientType+".json");
+        if("Donor".equals(ClientType)){
+            type =  new TypeToken<LinkedList<Donor>>() {}.getType();
+        }
+        else{
+            type = new TypeToken<LinkedList<Recipient>>() {}.getType();
+        }
+        String json = gson.toJson(L, type);
+        try{
+            FileWriter fw = new FileWriter (""+ClientType+".json");
+            fw.write(json);
+            fw.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public static LinkedList loadList (String fileName){
+        Gson gson = new Gson ();
+        Type type;
+        System.out.println("Reading file "+fileName);
+        switch (fileName){
+            case "Donor.json":
+                try{
+                    type = new TypeToken<LinkedList<Donor>>() {}.getType();
+                    BufferedReader br = new BufferedReader(new FileReader(""+fileName));
+                    LinkedList <Donor> L = new LinkedList (gson.fromJson(br, type));
+                    return L;
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+                break;
+            case "Recipient.json":
+                try{
+                    type = new TypeToken<LinkedList<Recipient>>() {}.getType();
+                    BufferedReader br = new BufferedReader(new FileReader(""+fileName));
+                    LinkedList <Recipient> L = new LinkedList (gson.fromJson(br, type));
+                    return L;
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+                break;   
+        }
+        return new LinkedList();
+    }
 }
