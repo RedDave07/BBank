60d7deb5d65420d046b57f64d26cb53f7e98f0f0
David <David@LAPTOP-DAVID>
2016-12-20 23:56:11 +0200

    save & loading are working now

--- src/bbank/BloodStock.java
+++ src/bbank/BloodStock.java
@@ -2,6 +2,8 @@ package bbank;
 
 import java.util.LinkedList;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -14,7 +16,17 @@ public class BloodStock {
     public static void saveListToFile (LinkedList L, String bloodType){
         Gson gson = new Gson ();
         System.out.println("saving to file "+""+bloodType+".json");
-        String json = gson.toJson(L);
+        Type type;
+        if("BloodA".equals(bloodType)){
+            type =  new TypeToken<LinkedList<BloodA>>() {}.getType();
+        }
+        else if("BloodB".equals(bloodType)){
+            type =  new TypeToken<LinkedList<BloodB>>() {}.getType();
+        }
+        else{
+            type =  new TypeToken<LinkedList<BloodO>>() {}.getType();
+        }
+        String json = gson.toJson(L, type);
         try{
             FileWriter fw = new FileWriter (""+bloodType+".json");
             fw.write(json);
@@ -24,15 +36,17 @@ public class BloodStock {
             e.printStackTrace();
         }
     }
-    
+
     public static LinkedList loadListFromFile (String fileName){
         Gson gson = new Gson ();
+        Type type;
         System.out.println("Reading file "+fileName);
         switch (fileName){
             case "BloodA.json":
                 try{
+                    type =  new TypeToken<LinkedList<BloodA>>() {}.getType();
                     BufferedReader br = new BufferedReader(new FileReader(""+fileName));
-                    LinkedList <BloodA> L = new LinkedList (gson.fromJson(br, LinkedList.class));
+                    LinkedList <BloodA> L = new LinkedList (gson.fromJson(br, type));
                     return L;
                 }
                 catch(Exception e){
@@ -41,8 +55,9 @@ public class BloodStock {
                 break;
             case "BloodB.json":
                 try{
+                    type =  new TypeToken<LinkedList<BloodB>>() {}.getType();
                     BufferedReader br = new BufferedReader(new FileReader(""+fileName));
-                    LinkedList <BloodB> L = new LinkedList (gson.fromJson(br, LinkedList.class));
+                    LinkedList <BloodB> L = new LinkedList (gson.fromJson(br, type));
                     return L;
                 }
                 catch(Exception e){
@@ -51,8 +66,9 @@ public class BloodStock {
                 break;
             case "BloodO.json":
                 try{
+                    type =  new TypeToken<LinkedList<BloodO>>() {}.getType();
                     BufferedReader br = new BufferedReader(new FileReader(""+fileName));
-                    LinkedList <BloodO> L = new LinkedList (gson.fromJson(br, LinkedList.class));
+                    LinkedList <BloodO> L = new LinkedList (gson.fromJson(br, type));
                    return L;
                 }
                 catch(Exception e){
