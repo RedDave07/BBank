a69901c1460f0c11aab27b169198838409161343
David <David@LAPTOP-DAVID>
2016-12-25 15:19:48 +0200

    new commit

--- src/bbank/BBank.java
+++ src/bbank/BBank.java
@@ -3,39 +3,33 @@ package bbank;
 public class BBank {
 
     public static void main(String[] args) {
-       System.out.println("maisnad fuck you"); 
-
-
-       Recipient r1 = new Recipient ("David","01111231072","A",55);
-       Recipient r2 = new Recipient ("Osama","01220006326","B",66);
-       ClientDataBase.Recipients_list.add(r1);
-       ClientDataBase.Recipients_list.add(r2);
-       
-              System.out.println("maisnad fuck you"); 
-
-       for(int i=0; i<ClientDataBase.Recipients_list.size(); i++){
-           System.out.println(ClientDataBase.Recipients_list.get(i).Name);
-       }
+//       Recipient r1 = new Recipient ("David","01111231072","A",55);
+//       Recipient r2 = new Recipient ("Osama","01220006326","B",66);
+//       ClientDataBase.Recipients_list.add(r1);
+//       ClientDataBase.Recipients_list.add(r2);
+//
+//       for(int i=0; i<ClientDataBase.Recipients_list.size(); i++){
+//           System.out.println(ClientDataBase.Recipients_list.get(i).Name);
+//       }
 //       System.out.println("Donors size "+ClientDataBase.Recipients_list.size());
-              System.out.println("maisnad fuck you"); 
-
-       ClientDataBase.saveList(ClientDataBase.Recipients_list, "Recipient");
-       ClientDataBase.Recipients_list.clear();
+//
+//       ClientDataBase.saveList(ClientDataBase.Recipients_list, "Recipient");
+//       ClientDataBase.Recipients_list.clear();
 //       System.out.println("Donors size "+ClientDataBase.Recipients_list.size());
-              System.out.println("maisnad fuck you"); 
+//
+//       ClientDataBase.Recipients_list = ClientDataBase.loadList("Recipient.json");
+//       System.out.println("Donors size "+ClientDataBase.Recipients_list.size());
+//       for(int i=0; i<ClientDataBase.Recipients_list.size(); i++){
+//           System.out.println(ClientDataBase.Recipients_list.get(i).Name);
+//       }
 
-       ClientDataBase.Recipients_list = ClientDataBase.loadList("Recipient.json");
-       System.out.println("Donors size "+ClientDataBase.Recipients_list.size());
-       for(int i=0; i<ClientDataBase.Recipients_list.size(); i++){
-           System.out.println(ClientDataBase.Recipients_list.get(i).Name);
-       }
-              System.out.println("maisnad fuck you"); 
-
-//       Window w = new Window();
-//       w.setVisible(true);
-        
-      
-       
+        Window w = new Window();
+        w.setVisible(true);
+        ClientDataBase.Donors_list = ClientDataBase.loadList("Donor.json");
+        ClientDataBase.Recipients_list = ClientDataBase.loadList("Recipient.json");
+        BloodStock.blood_A = BloodStock.loadListFromFile("BloodA.json");
+        BloodStock.blood_B = BloodStock.loadListFromFile("BloodB.json");
+        BloodStock.blood_O = BloodStock.loadListFromFile("BloodO.json");
     }
     
 }
